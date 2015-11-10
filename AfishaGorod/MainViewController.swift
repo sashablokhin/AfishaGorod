c89a83589c1d9df22668da9ddc457abc228e1c3e
sashablokhin <sashablokhindev@gmail.com>
2015-11-10 16:23:48 +0300

    Added animation of the menu cells

--- AfishaGorod/MainViewController.swift
+++ AfishaGorod/MainViewController.swift
@@ -15,7 +15,7 @@ class MainViewController: UIViewController {
         
         let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
         imageView.contentMode = .ScaleAspectFit
-        imageView.image = UIImage(named: "gorod")
+        imageView.image = UIImage(named: "gorod2")
         
         navigationItem.titleView = imageView
     
--- AfishaGorod/MenuTableViewController.swift
+++ AfishaGorod/MenuTableViewController.swift
@@ -14,20 +14,42 @@ class MenuTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //menuItems = ["Главная", "Перемены", "Люди", "Дома", "Еда", "Вещи", "Развлечения"]
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+    
+    override func viewWillAppear(animated: Bool) {
+        animateTable()
+    }
+    
+    func animateTable() {
+        tableView.reloadData()
+        
+        let cells = tableView.visibleCells
+        let tableHeight: CGFloat = tableView.bounds.size.height
+        
+        for i in cells {
+            let cell: UITableViewCell = i as UITableViewCell
+            cell.transform = CGAffineTransformMakeTranslation(0, tableHeight)
+        }
+        
+        var index = 0
+        
+        for a in cells {
+            let cell: UITableViewCell = a as UITableViewCell
+            
+            UIView.animateWithDuration(1.0, delay: 0.05 * Double(index), usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: UIViewAnimationOptions.AllowAnimatedContent, animations: { () -> Void in
+                cell.transform = CGAffineTransformMakeTranslation(0, 0)
+                }, completion: nil)
+            
+            index += 1
+        }
+    }
+    
+    
 
     // MARK: - Table view data source
 
@@ -35,20 +57,7 @@ class MenuTableViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-/*
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return tableView.numberOfRowsInSection(section)//menuItems.count
-    }*/
-
-    /*
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
-
-        cell.textLabel?.text = menuItems[indexPath.row]
-
-        return cell
-    }*/
+    
     
 
     /*
