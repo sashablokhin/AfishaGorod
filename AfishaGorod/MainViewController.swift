b6d2acf4bb471ba2bbcd504b6325bb26e1cc8df1
sashablokhin <sashablokhindev@gmail.com>
2015-11-11 22:09:14 +0300

    Parse posts

--- AfishaGorod/MainViewController.swift
+++ AfishaGorod/MainViewController.swift
@@ -6,9 +6,12 @@
 //  Copyright © 2015 Alexander Blokhin. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
 class MainViewController: UIViewController {
+    
+    let baseUrlString = "http://gorod.afisha.ru"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,6 +37,8 @@ class MainViewController: UIViewController {
         view.addGestureRecognizer(revealViewController().tapGestureRecognizer())
         
         revealViewController().rearViewRevealWidth = 130
+        
+        request()
     }
 
     override func didReceiveMemoryWarning() {
@@ -42,5 +47,131 @@ class MainViewController: UIViewController {
     }
 
 
+    func request() {
+        let url = NSURL(string: baseUrlString)
+        
+        if url != nil {
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: { (data, response, error) -> Void in
+                
+                //var urlError = false
+                
+                if error == nil {
+                    
+                    let content = NSString(data: data!, encoding: NSUTF8StringEncoding) as NSString!
+                    
+                    let contentArray: NSArray = content.componentsSeparatedByString("<article class=") as NSArray!
+                    
+                    if contentArray.count > 1 {
+                        for i in 1..<contentArray.count {
+                            self.parseArticle(contentArray[i] as! String)
+                        }
+                    } else {
+                        //urlError = true
+                    }
+                    
+                } else {
+                    //urlError = true
+                }
+            })
+            
+            task.resume()
+            
+        } else {
+            //showError()
+        }
+    }
+    
+    
+    func parseArticle(var article: String) {
+        if article.containsString("articles_item") {
+            if article.containsString("articles_super") {
+                
+                let link = article.valueForTag("<a href=\"", close: "\"")
+                let title = article.valueForTag("title=\"", close: "\"")
+                let img = article.valueForTag("data-src1=\"", close: "\"")
+                let theme = article.valueForTag("article_theme\">", close: "</span>")
+                let views = article.valueForTag("article-info_views\">", close: "</span>")
+                let comments = article.valueForTag("article-info_comments\">", close: "</span>")
+                let tag = article.valueForTag("title=\"", close: "\"")
+                let datetime = article.valueForTag("datetime=\"", close: "</time>")//.componentsSeparatedByString("\">").last
+                let fullDateTime = datetime.componentsSeparatedByString("\">").first!
+                let shortDateTime = datetime.componentsSeparatedByString("\">").last!
+                
+                print(link)
+                print(title)
+                print(img)
+                print(theme)
+                print(views)
+                print(comments)
+                print(tag)
+                print(fullDateTime)
+                print(shortDateTime)
+                print("\n")
+                
+            } else {
+                let link = article.valueForTag("<a href=\"", close: "\"")
+                let img = article.valueForTag("<img src=\"", close: "\"")
+                let tag = article.valueForTag("class=\"tag", close: "<").componentsSeparatedByString(">").last!
+                let datetime = article.valueForTag("datetime=\"", close: "</time>")
+                let fullDateTime = datetime.componentsSeparatedByString("\">").first!
+                let shortDateTime = datetime.componentsSeparatedByString("\">").last!
+                let title = article.valueForTag("title=\"", close: "\">")
+                let theme = article.substringToIndex((article.rangeOfString("<span")?.startIndex)!)
+                let views = article.valueForTag("article-info_views\">", close: "</span>")
+                let comments = article.valueForTag("article-info_comments\">", close: "</span>")
+                
+                print(link)
+                print(title)
+                print(img)
+                print(tag)
+                print(fullDateTime)
+                print(shortDateTime)
+                print(theme)
+                print(views)
+                print(comments)
+                print("\n")
+            }
+        }
+    }
 }
+
+extension String {
+    mutating func valueForTag(tag: String, close: String) -> String {
+        let start = self.rangeOfString(tag)?.endIndex
+        
+        self = self.substringFromIndex(start!)
+        
+        let startClose = self.rangeOfString(close)?.startIndex
+        let endClose = self.rangeOfString(close)?.endIndex
+        
+        let value = self.substringToIndex(startClose!)
+        
+        self = self.substringFromIndex(endClose!)
+        
+        return value
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
